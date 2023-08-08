1e3a4a425bb70b33a0e3c1309d754bc376cccaa1
passowrd1234 <141778553+passowrd1234@users.noreply.github.com>
2023-08-09 02:27:03 +0530

    Update igban.py

--- igban.py
+++ igban.py
@@ -1,23 +1,23 @@
 import requests
 
 def login(username, password):
-    # Same as before...
+    # (igservicemaster,shiru@@12)
 
 def report_user(session, user_id):
-    # Same as before...
+    # (its.still.alex)
 
 if __name__ == "__main__":
     # Replace 'your_username' and 'your_password' with your actual Instagram credentials
-    your_username = 'your_username'
-    your_password = 'your_password'
+    your_username = 'igservicemaster'
+    your_password = 'shiru@@12'
     
-    user_id_to_report = '123456789'  # Replace '123456789' with the user ID of the profile you want to report
-    num_reports = 100
+    user_id_to_report = 'its.still.alex'  # Replace '123456789' with the user ID of the profile you want to report
+    num_reports = 1000
 
-    session = login(your_username, your_password)
+    session = login(igservicemaster,shiru@@12)
 
-    for i in range(num_reports):
-        report_user(session, user_id_to_report)
+    for i in range(num_reports):1000
+        report_user(session,its.still.alex)
         print(f"Report {i + 1} completed.")
 
     print("All reports completed.")
